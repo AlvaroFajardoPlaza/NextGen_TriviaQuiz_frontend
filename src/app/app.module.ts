7ae7993646d4b64d0f276fc7ddc58b5eb8803f71
alvaro <alvaro.fajardoplaz@gmail.com>
2024-01-30 16:02:01 +0100

    configuración inicial proyecto de front en Angular. Estructura de proyecto, vistas compartidas de navbar, footer y homepage inicial listas.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/views/shared_views/nav/nav.component';
+import { FooterComponent } from './components/views/shared_views/footer/footer.component';
+import { HomeComponent } from './components/views/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/views/shared_views/nav/nav.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.scss']
+})
+export class NavComponent {
+  x: any
+
+  myFunction() {
+    this.x = document.getElementById("nav_bar");
+    if (this.x.className === "topnav") {
+      this.x.className += " responsive";
+    } else {
+      this.x.className = "topnav";
+    }
+  }
+}
