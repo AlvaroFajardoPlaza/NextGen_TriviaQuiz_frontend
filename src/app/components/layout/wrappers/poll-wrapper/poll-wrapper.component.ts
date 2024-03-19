ff4a6d3c1f9f06f1c4037a6fe79d6f025cf41c92
alvaro <alvaro.fajardoplaz@gmail.com>
2024-03-19 16:20:25 +0100

    ajustes en la searchbar y estructura de la tabla en componente poll para registrar las puntuaciones de los usuarios.

--- src/app/components/layout/wrappers/poll-wrapper/poll-wrapper.component.ts
+++ src/app/components/layout/wrappers/poll-wrapper/poll-wrapper.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, inject } from '@angular/core';
-import { Observable } from 'rxjs';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Observable, map } from 'rxjs';
 import { AuthService } from 'src/app/components/auth/auth.service';
 import { User } from 'src/app/data/models/User';
 
@@ -8,8 +9,43 @@ import { User } from 'src/app/data/models/User';
 	templateUrl: './poll-wrapper.component.html',
 	styleUrls: ['./poll-wrapper.component.scss']
 })
-export class PollWrapperComponent {
+export class PollWrapperComponent implements OnInit {
 	private _authSvc = inject(AuthService);
+	private _fb = inject(FormBuilder);
 
 	allUsers$: Observable<Array<User>> | any = this._authSvc.getAllUsers();
+	filteredUsersList$: Observable<Array<User>> | any;
+	searchForUserForm: FormGroup | any;
+
+	constructor() {
+		// Creamos nuestra search bar desde el constructor del componente
+		this.searchForUserForm = this._fb.group({
+			text: [null, Validators.required]
+		});
+	}
+
+	ngOnInit(): void {
+		this.filterUsers('');
+	}
+
+	// Funcionalidad para la Search Bar ----------------------------------------------------------------------------------------
+	filterUsers(text: string) {
+		console.log('Tenemos algo en el input?', text);
+		if (!text) {
+			console.log('No se ha introducido nada en el input');
+			this.filteredUsersList$ = this.allUsers$;
+			return;
+		} else {
+			// Tenemos que plantear un filter dentro del observable
+			this.filteredUsersList$ = this.allUsers$.pipe(
+				map((users: Array<User>) =>
+					users.filter((user: User) =>
+						user.username
+							?.toLowerCase()
+							.includes(text.toLowerCase())
+					)
+				)
+			);
+		}
+	}
 }
